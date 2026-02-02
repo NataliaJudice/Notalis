8bf04c64ed760e9abbb981f6a91837bdb8ccecad
Joan Cuauhtemoc Farias Alor <joancuauc@gmail.com>
2026-02-02 15:10:55 -0300

    AMEM

---
+++ NotasMoodle.Banco/LoginContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using NotasMoodle.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class LoginContext : IdentityDbContext
+{
+    public LoginContext(DbContextOptions<LoginContext> options)
+        : base(options)
+    {
+    }
+
+ 
+}
